38270abc43951d08db2f2fd113e0253506c9c90e
j <77560326+Jxl-s@users.noreply.github.com>
2024-02-27 22:50:21 -0500

    add stars

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Bounds, OrbitControls, Stage, useProgress } from "@react-three/drei";
+import { Bounds, OrbitControls, Stage, Stars, useProgress } from "@react-three/drei";
 import { Scene } from "./Scene";
 import { Robot } from "./objects/Robot";
 import { Perf } from "r3f-perf";
@@ -20,8 +20,10 @@ export default function Experience() {
         <axesHelper />
         <Perf />
         <Robot scale={[4, 4, 4]} />
+        <Stars radius={25} depth={50} count={500} factor={5} saturation={0} fade speed={2} />
+
         <Suspense fallback={<Progress />}>
-            <Bounds fit observe margin={1}>
+            <Bounds fit observe margin={0.9}>
                 <Scene scale={[4, 4, 4]} />
             </Bounds>
         </Suspense>
