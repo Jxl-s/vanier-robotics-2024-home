6696ccb14b21b0b01fc58d4836d2dda114e974b9
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-02-28 20:11:27 -0500

    add youtube video

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -55,12 +55,13 @@ export function Scene(props) {
       {/* <mesh geometry={nodes.TV1Screen.geometry} material={commonMaterial} position={[-0.869, 0.83, -0.227]} rotation={[Math.PI / 2, 1.396, -Math.PI / 2]} /> */}
       {/* <mesh geometry={nodes.TV2Screen.geometry} material={commonMaterial} position={[1.636, 0.83, -1.283]} rotation={[0.175, 0, 0]} /> */}
 
-      <TVScreen position={[-0.869, 0.83, -0.227]} rotation={[Math.PI / 2, 1.396, -Math.PI / 2]} video="/videos/camera_1.mp4"
+      <TVScreen position={[-0.869, 0.83, -0.227]} rotation={[Math.PI / 2, 1.396, -Math.PI / 2]} video="/embed/tgbNymZ7vqY"
         powerProps={{
           geometry: nodes.TV1Power.geometry,
           position: [-0.898, 0.561, -0.6],
           rotation: [Math.PI / 2, 1.396, -Math.PI / 2]
         }}
+        isYoutube={true}
         lightProps={{
           geometry: nodes.TV1Light.geometry,
           position: [-0.898, 0.561, -0.555],
--- src/objects/TVScreen.jsx
+++ src/objects/TVScreen.jsx
@@ -1,6 +1,6 @@
-import { useVideoTexture } from "@react-three/drei";
+import { Html, useVideoTexture } from "@react-three/drei";
 import * as THREE from "three";
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import gsap from 'gsap';
 
 const SCREEN_DIMENSIONS = [0.77 * 1.05, 0.453 * 1.05];
@@ -14,12 +14,19 @@ export default function TVScreen({
     rotation,
 
     powerProps,
-    lightProps
+    lightProps,
+    isYoutube = false,
 }) {
     const [tvEnabled, setTvEnabled] = useState(true);
+    const iframeRef = useRef();
 
-    const videoTexture = useVideoTexture(video);
-    const tvMaterial = useMemo(() => new THREE.MeshBasicMaterial({ color: SCREEN_COLOR, map: videoTexture }), [videoTexture]);
+    let tvMaterial;
+    if (!isYoutube) {
+        const videoTexture = useVideoTexture(video);
+        tvMaterial = useMemo(() => new THREE.MeshBasicMaterial({ color: SCREEN_COLOR, map: videoTexture }), [videoTexture]);
+    } else {
+        tvMaterial = useMemo(() => new THREE.MeshBasicMaterial({ color: 0x000000 }), []);
+    }
 
     const powerMaterial = useMemo(() => new THREE.MeshBasicMaterial({ color: 0xff0000 }), []);
     const lightMaterial = useMemo(() => new THREE.MeshBasicMaterial({ color: 0xffffff }), []);
@@ -41,10 +48,36 @@ export default function TVScreen({
         gsap.to(lightMaterial.color, { duration: 0.2, r: newLightColor, g: newLightColor, b: newLightColor });
 
         tvMaterial.color = tvEnabled ? new THREE.Color(SCREEN_COLOR) : new THREE.Color(0x000000);
+        if (iframeRef.current) {
+            iframeRef.current.style.display = tvEnabled ? "block" : "none";
+        }
+
     }, [tvEnabled, lightMaterial, tvMaterial]);
 
     return <>
-        <mesh position={position} rotation={rotation} geometry={TVGeometry} material={tvMaterial} scale={[SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1], 1]} />
+        {isYoutube ?
+            <>
+                <Html position={position} rotation={rotation} material={tvMaterial} transform center distanceFactor={0.5}>
+                    <iframe
+                        src={'https://www.youtube.com' + video + '?autoplay=1&mute=1&controls=0'}
+                        title="YouTube"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        style={{
+                            height: "380px",
+                            width: "660px",
+                            border: "none",
+                            pointerEvents: "none",
+                            display: "block"
+                        }}
+                        ref={iframeRef}
+                    />
+                </Html>
+
+                <mesh position={position} rotation={rotation} geometry={TVGeometry} material={tvMaterial} scale={[SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1], 1]} />
+            </>
+            :
+            <mesh position={position} rotation={rotation} geometry={TVGeometry} material={tvMaterial} scale={[SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1], 1]} />
+        }
         <mesh {...powerProps} material={powerMaterial} onPointerOver={onPowerHover} onPointerLeave={onPowerLeave} onClick={onPowerClick} />
         <mesh {...lightProps} material={lightMaterial} />
     </>
